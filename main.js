4041152f16d8c089f2bf214d7774e44aaf979d0f
pbsinclair42 <bobsmith0030+git2@gmail.com>
2016-11-12 13:32:51 +0000

    Add first task

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 storyFadeLength = 5000;
 
+taskLengths = {'remember':10}
+
 function nextStoryline(lines){
     s = $(".story");
     s.html(lines.shift());
     s.css('animation-name', 'none')
     setTimeout(function(){s.css('animation-name', 'storyAnimation')},10)
     if (lines.length > 0){
-        setTimeout(function(){nextStoryline(lines)}, storyFadeLength)
+        setTimeout(function(){
+            nextStoryline(lines);
+        }, storyFadeLength)
+    } else {
+        setTimeout(function(){
+        }, storyFadeLength)
     }
 }
 
 $(document).ready(function(){
+
+    $('.task').click(function(){
+        task = $(this)
+        task.addClass('waiting');
+        task.css('animation-duration',taskLengths[task.attr('id')]+'s')
+        task.prop('disabled', true);
+        setTimeout(function(){
+            task.removeClass('waiting');
+            task.prop('disabled', false);
+        }, taskLengths['remember']*1000)
+    })
+    
     nextStoryline(['You wake up.', 'You are in darkness.'])
 })
 
